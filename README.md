d773636b69d6da4044522b2be1f0f4e0577d55bb
stormmurdoc <murdoc@storm-clan.de>
2021-11-01 16:55:38 +0100

    initial checkin

---
+++ README.md
@@ -0,0 +1,26 @@
+# Kubernetes in Docker (KinD)
+
+This project compiles a local Docker container and then deploys it to the
+kind cluster. Afterwards, the application and the service are deployed
+in the KinD-k8s cluster.
+
+## Prerequisites
+
+The following components must be installed:
+
+- docker
+- kind
+- kubectl
+
+## Deploy
+
+![image_2021-11-01-16-48-39](img/image_2021-11-01-16-48-39.png)
+
+Start the deployment with
+
+        deploy.sh
+
+## Destroy cluster
+Getting rid of a KinD cluster
+
+        kind delete cluster
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+RESULT=$(kind get clusters)
+SVC_NAME=nginx-service
+
+if [ -z "$RESULT" ];then
+    echo "No kind cluster found! - starting..."
+    kind create cluster --config kind.yaml
+else
+    echo "cluster '$RESULT' found!"
+fi
+
+echo "+++ build docker image +++"
+docker build . -t blog:0.1.0
+
+echo "+++ loading docker image into kind +++"
+kind load docker-image blog:0.1.0
+
+#kind delete cluster
+kubectl apply -f nginx-deployment.yaml
+kubectl apply -f nginx-service.yaml
+sleep 5
+NODE_IP=$(kubectl get node -o wide|tail -1|awk {'print $6'})
+NODE_PORT=$(kubectl get svc "$SVC_NAME" -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
+echo "http://$NODE_IP:$NODE_PORT"
+RESULT=$(curl $NODE_IP:$NODE_PORT)
+echo "------------------------------------------------------------------------"
+echo "$RESULT"
+echo "------------------------------------------------------------------------"
